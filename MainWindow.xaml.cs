29f99ba99cde7eba786cf47fe37f476d6140a3d9
leandro ostruka <leandro.sakae@hotmail.com>
2023-12-28 20:30:49 -0300

    Add Max functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -19,12 +21,24 @@ namespace StockSystem
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
 
         private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
+            {
+                WindowInteropHelper helper = new WindowInteropHelper(this);
+                SendMessage(helper.Handle, 0x112, 0xf012, 0);
+            }
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
 
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
@@ -43,6 +57,6 @@ namespace StockSystem
         {
             Application.Current.Shutdown();
         }
-
+        
     }
 }
