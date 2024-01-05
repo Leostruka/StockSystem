e97e837c591b7ae2d0a3a8e4191886b905b2b241
leandro ostruka <leandro.sakae@hotmail.com>
2024-01-05 15:20:35 -0300

    Att SQL

--- SqlConfig/CreateTables.sql
+++ SqlConfig/CreateTables.sql
@@ -1,56 +1,110 @@
--- Create database
-CREATE DATABASE `stockdata`;
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
 
--- Tabela de Usuários
-CREATE TABLE `employee` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50),
-    `cpf` VARCHAR(11),
-    `email` VARCHAR(256),
-    `user` VARCHAR(50),
-    `password` VARCHAR(255)   
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Tabela de Permissões
-CREATE TABLE `permission` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50)
-);
 
--- Tabela de Vínculo entre Usuários e Permissões
-CREATE TABLE `userpermission` (
-    `userid` INT,
-    `permissionid` INT,
-    PRIMARY KEY (`userid`, `permissionid`),
-    FOREIGN KEY (`userid`) REFERENCES `employee`(`id`),
-    FOREIGN KEY (`permissionid`) REFERENCES `permission`(`id`)
-);
+-- Copiando estrutura do banco de dados para stockdata
+CREATE DATABASE IF NOT EXISTS `stockdata` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `stockdata`;
 
--- Tabela de Itens do Estoque
-CREATE TABLE `stock` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50),
-    `amount` INT,
-    `price` DECIMAL(8,2),
-    `descript` VARCHAR(200)
-);
+-- Copiando estrutura para tabela stockdata.employee
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `user` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`) USING BTREE,
+  UNIQUE KEY `cpf` (`cpf`),
+  UNIQUE KEY `user` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Tabela de Transações do Estoque
-CREATE TABLE `stocktransaction` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `stockid` INT,
-    `userid` INT,
-    `type` VARCHAR(10),
-    `amount` INT,
-    `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (`stockid`) REFERENCES `stock`(`id`),
-    FOREIGN KEY (`userid`) REFERENCES `employee`(`id`)
-);
+-- Exportação de dados foi desmarcado.
 
-CREATE TABLE `supplier` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(144) NULL DEFAULT NULL,
-    `cpf_cnpj` VARCHAR(14) NULL DEFAULT NULL,
-    `email` VARCHAR(256) NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
+-- Copiando estrutura para tabela stockdata.permission
+CREATE TABLE IF NOT EXISTS `permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela stockdata.stock
+CREATE TABLE IF NOT EXISTS `stock` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `supplierid` int DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `price` decimal(8,2) DEFAULT NULL,
+  `descript` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `supplierid` (`supplierid`),
+  CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`supplierid`) REFERENCES `supplier` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela stockdata.stocktransaction
+CREATE TABLE IF NOT EXISTS `stocktransaction` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `stockid` int DEFAULT NULL,
+  `userid` int DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `amount` int DEFAULT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `stockid` (`stockid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `stocktransaction_ibfk_1` FOREIGN KEY (`stockid`) REFERENCES `stock` (`id`),
+  CONSTRAINT `stocktransaction_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `employee` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela stockdata.supplier
+CREATE TABLE IF NOT EXISTS `supplier` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(144) DEFAULT NULL,
+  `cpf_cnpj` varchar(14) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cpf_cnpj` (`cpf_cnpj`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+-- Copiando estrutura para tabela stockdata.userpermission
+CREATE TABLE IF NOT EXISTS `userpermission` (
+  `userid` int NOT NULL,
+  `permissionid` int NOT NULL,
+  PRIMARY KEY (`userid`,`permissionid`),
+  KEY `permissionid` (`permissionid`),
+  CONSTRAINT `userpermission_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `employee` (`id`),
+  CONSTRAINT `userpermission_ibfk_2` FOREIGN KEY (`permissionid`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Exportação de dados foi desmarcado.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
